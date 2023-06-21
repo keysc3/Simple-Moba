897bea51d22d96ed8b58b7a95324782f04f3d012
keysc3 <colin.j.keys@gmail.com>
2023-06-20 18:54:08 -0600

    Simplified Billia's spell 1 radius checking and passive logic.

--- Assets/Scripts/Billia/BilliaAbilities.cs
+++ Assets/Scripts/Billia/BilliaAbilities.cs
@@ -17,10 +17,6 @@ public class BilliaAbilities : ChampionAbilities
        spell_1_passiveStacks = 0;
     }
 
-    void Update(){
-        Debug.Log("Passive stacks: " + spell_1_passiveStacks);
-    }
-
     /*
     *   Spell_1 - Sets up Billia's first spell. She swirls her weapon in a radius around her. Players hit by the outer portion take bonus damage.
     *   Passive: Gain a stacking speed bonus whenever a unit is hit with any spell, up to 4 stacks.
@@ -44,24 +40,41 @@ public class BilliaAbilities : ChampionAbilities
         while(isCasting)
             yield return null;
         LayerMask enemyMask = LayerMask.GetMask("Enemy");
-        //TODO: Fix outerHit to not include inner radius.
         List<Collider> outerHit = new List<Collider>(Physics.OverlapSphere(transform.position, billia.spell_1_outerRadius, enemyMask));
-        List<Collider> innerHit = new List<Collider>(Physics.OverlapSphere(transform.position, billia.spell_1_innerRadius, enemyMask));
         foreach(Collider collider in outerHit){
-            //billiaAbilityHit.Spell_1_Hit(collider.gameObject, "outer");
-            //TODO: Add passive dot.
-            passiveStack = true;
-        }
-        foreach(Collider collider in innerHit){
-            if(!outerHit.Contains(collider)){
+            // Check if the unit was hit only by the inner portion.
+            if(CheckSpell_1_OnlyInnerHit(collider)){
+                Debug.Log("inner radius hit");
                 //billiaAbilityHit.Spell_1_Hit(collider.gameObject, "inner");
-                //TODO: Add passive dot.
-                passiveStack = true;
+                // TODO: Add passive dot.
             }
+            // Unit hit by outer portion.
+            else{
+                Debug.Log("outer radius hit");
+                //billiaAbilityHit.Spell_1_Hit(collider.gameObject, "outer");
+                // TODO: Add passive dot.
+            }
+            passiveStack = true;
         }
         // If a unit was hit proc the spells passive.
         if(passiveStack)
             Spell_1_PassiveProc();
+    }
+
+    /*
+    *   CheckSpell_1_OnlyInnerHit - Checks if a colliders min and max bounds are both within the inner radius of spell 1.
+    *   @param collider - Collider to check the bounds of.
+    */
+    private bool CheckSpell_1_OnlyInnerHit(Collider collider){
+        // Get the min and max bounds of the collider and set their y to the same as Billia.
+        Vector3 min = collider.bounds.min;
+        Vector3 max = collider.bounds.max;
+        max.y = transform.position.y;
+        min.y = transform.position.y;
+        // Get the distances from Billia and return whether or not both are within spell 1's inner radius.
+        float minMag = (transform.position - min).magnitude;
+        float maxMag = (transform.position - max).magnitude;
+        return minMag <= billia.spell_1_innerRadius && maxMag <= billia.spell_1_innerRadius;
     }
 
     /*
@@ -80,7 +93,7 @@ public class BilliaAbilities : ChampionAbilities
     }
 
     /*
-    *   Spell_1_PassiveRunning - Controls logic for whether the passive is still in the running of not.
+    *   Spell_1_PassiveRunning - Controls logic for whether the passive is still running or not.
     */
     private IEnumerator Spell_1_PassiveRunning(){
         spell_1_passiveRunning = true;
@@ -89,6 +102,7 @@ public class BilliaAbilities : ChampionAbilities
             yield return null;
         }
         spell_1_passiveRunning = false;
+        // Initiate stack fall off coroutine.
         StartCoroutine(Spell_1_PassiveDropping());
     }
 
@@ -98,7 +112,6 @@ public class BilliaAbilities : ChampionAbilities
     private IEnumerator Spell_1_PassiveDropping(){
         // While spell 1 passive has stopped running drop a stack every iteration.
         while(!spell_1_passiveRunning && spell_1_passiveStacks > 0){
-            Debug.Log("Passive stacks dropping");
             //TODO: Take away speed boost.
             spell_1_passiveStacks -= 1;
             yield return new WaitForSeconds(billia.spell_1_passiveExpireDuration);
