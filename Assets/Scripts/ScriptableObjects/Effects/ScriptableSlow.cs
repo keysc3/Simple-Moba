55728cda18a139d3299d6b84d071235e97569053
keysc3 <colin.j.keys@gmail.com>
2023-07-04 15:55:04 -0600

    Documentation.

--- Assets/Scripts/ScriptableObjects/Effects/ScriptableSlow.cs
+++ Assets/Scripts/ScriptableObjects/Effects/ScriptableSlow.cs
@@ -18,11 +18,19 @@ public class ScriptableSlow : ScriptableEffect
         return new Slow(this, duration, unitCasted, unitEffected);
     }
 
-
+    /*
+    *   SetDuration - Sets the slows duration.
+    *   @param duration - float of duration to set slow to.
+    */
     public void SetDuration(float duration){
         this.duration = duration;
     }
 
+    /*
+    *   isEffectStronger - Checks if the the second effect is stronger than the first.
+    *   @param strongest - ScriptableSlow of a slow.
+    *   @param newSlow - ScriptableSlow of the slow to check if stronger.
+    */
     public bool isEffectStronger(ScriptableSlow strongest, ScriptableSlow newSlow){
         if(newSlow.slowPercent > strongest.slowPercent)
             return true;
@@ -30,6 +38,11 @@ public class ScriptableSlow : ScriptableEffect
             return false;
     }
 
+    /*
+    *   GetStrongest - Finds the index of the strongest slow. GetStrongest is only called if a slow exists in the list given.
+    *   @param myEffects - List containing all effects to check.
+    *   @return int - int of the index the strongest slow is at.
+    */
     public int GetStrongest(List<Effect> myEffects){
         int strongestIndex = -1;
         int i = 0;
