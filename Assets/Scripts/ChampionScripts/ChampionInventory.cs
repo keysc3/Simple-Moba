2450701fe388f4f9c88da30b961e1589a7878a01
keysc3 <colin.j.keys@gmail.com>
2023-07-12 12:58:42 -0600

    Move and refactor item stat updates.

--- Assets/Scripts/ChampionScripts/ChampionInventory.cs
+++ Assets/Scripts/ChampionScripts/ChampionInventory.cs
@@ -45,7 +45,7 @@ public class ChampionInventory : MonoBehaviour
                 if(!myItems.ContainsKey(i)){
                     itemSlot = i;
                     myItems.Add(itemSlot, item);
-                    championStats.InventoryChanged(item, "Buy"); 
+                    AddItemStats(item); 
                     uiManager.AddItem(itemSlot, item.icon);
                     championStats.UpdateAttackSpeed();
                     uiManager.UpdateAllStats();
@@ -68,7 +68,7 @@ public class ChampionInventory : MonoBehaviour
         if(myItems.ContainsKey(slot)){
             Item item = myItems[slot];
             myItems.Remove(slot);
-            championStats.InventoryChanged(item, "Sell");
+            RemoveItemStats(item);
             uiManager.RemoveItem(slot);
             championStats.UpdateAttackSpeed();
             uiManager.UpdateAllStats();
@@ -89,6 +89,64 @@ public class ChampionInventory : MonoBehaviour
                 displayItemsInsp.Add(myItems[i].name);
             else
                 displayItemsInsp.Add("No Item");
+        }
+    }
+
+    /*
+    *   AddItemStats - Add an items stats to the champions stats.
+    *   @param item - Item to that was added to the inventory.
+    */
+    private void AddItemStats(Item item){
+        if(item != null){
+            // Bought an item so add the items stats.
+            championStats.magicDamage.AddModifier(item.magicDamage);
+            championStats.physicalDamage.AddModifier(item.physicalDamage);
+            // Increase current health by items health amount.
+            if(item.health != 0){
+                championStats.maxHealth.AddModifier(item.health);
+                if(!championStats.isDead){
+                    championStats.SetHealth(championStats.currentHealth + item.health);
+                    GetComponent<UIManager>().UpdateHealthBar();
+                }
+            }
+            // Increase current mana by items mana amount.
+            if(item.mana != 0){
+                championStats.maxMana.AddModifier(item.mana);
+                if(!championStats.isDead){
+                    championStats.SetMana(championStats.currentMana + item.mana);
+                    GetComponent<UIManager>().UpdateManaBar();
+                }
+            }
+            championStats.speed.AddModifier(item.speed);
+            championStats.bonusAttackSpeed.AddModifier(item.attackSpeed);
+        }
+    }
+
+    /*
+    *   RemoveItemStats - Remove an items stats from the champions stats.
+    *   @param item - Item to that was added to the inventory.
+    */
+    private void RemoveItemStats(Item item){
+        if(item != null){
+            // Selling an item so remove the items stats.
+            championStats.magicDamage.RemoveModifier(item.magicDamage);
+            championStats.physicalDamage.RemoveModifier(item.physicalDamage);
+            //If the items hp hasn't been used yet, remove the amount that hasn't been used from current health.
+            if(item.health != 0){
+                if(championStats.currentHealth > (championStats.maxHealth.GetValue() - item.health))
+                    championStats.SetHealth(championStats.currentHealth - (item.health - (championStats.maxHealth.GetValue() - championStats.currentHealth))); 
+                championStats.maxHealth.RemoveModifier(item.health);
+                GetComponent<UIManager>().UpdateHealthBar();
+            }
+            //If the items mana hasn't been used yet, remove the amount that hasn't been used from the current mana.
+            if(item.mana != 0){
+                if(championStats.currentMana > (championStats.maxMana.GetValue() - item.mana))
+                    championStats.SetMana(championStats.currentMana - (item.mana - (championStats.maxMana.GetValue() - championStats.currentMana)));
+                championStats.maxMana.RemoveModifier(item.mana);
+                GetComponent<UIManager>().UpdateManaBar();
+            }
+            championStats.speed.RemoveModifier(item.speed);
+            championStats.bonusAttackSpeed.RemoveModifier(item.attackSpeed);
         }
     }
 }
