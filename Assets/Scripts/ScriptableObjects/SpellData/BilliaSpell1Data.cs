a4f259f589c7c933174c222486a0540e247d958a
keysc3 <colin.j.keys@gmail.com>
2023-07-22 16:04:21 -0600

    Billia spell1 spelldata.

---
+++ Assets/Scripts/ScriptableObjects/SpellData/BilliaSpell1Data.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BilliaSpell1", menuName = "Spell/Billia/Spell1")]
+public class BilliaSpell1Data : SpellData
+{
+    [field: SerializeField] public Sprite passiveSprite { get; private set; }
+    [field: SerializeField] public GameObject visualPrefab { get; private set; }
+    [field: SerializeField] public float initialAlpha { get; private set; } = 60.0f;
+    [field: SerializeField] public float finalAlpha { get; private set; } = 160.0f;
+    [field: SerializeField] public List<float> passiveSpeed { get; private set; }
+    [field: SerializeField] public float innerRadius { get; private set; }
+    [field: SerializeField] public float outerRadius { get; private set; }
+    [field: SerializeField] public float passiveSpeedDuration { get; private set; }
+    [field: SerializeField] public float passiveExpireDuration { get; private set; }
+    [field: SerializeField] public float passiveMaxStacks { get; private set; }
+}
