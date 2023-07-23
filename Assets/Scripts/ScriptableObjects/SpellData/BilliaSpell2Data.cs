515bc0fde9f9235db4fcbd74490e8157e04581cf
keysc3 <colin.j.keys@gmail.com>
2023-07-22 20:12:42 -0600

    Billia spell2 spelldata.

---
+++ Assets/Scripts/ScriptableObjects/SpellData/BilliaSpell2Data.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BilliaSpell2", menuName = "Spell/Billia/Spell2")]
+public class BilliaSpell2Data : SpellData
+{
+    [field: SerializeField] public GameObject visualPrefab { get; private set; }
+    [field: SerializeField] public float innerRadius { get; private set; }
+    [field: SerializeField] public float outerRadius { get; private set; }
+    [field: SerializeField] public float maxMagnitude { get; private set; }
+    [field: SerializeField] public float dashOffset { get; private set; }
+    [field: SerializeField] public float dashTime { get; private set; }
+}
