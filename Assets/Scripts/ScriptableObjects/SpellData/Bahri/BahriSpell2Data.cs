12ce62f88395130e39909d18c2ad8d36f2ad559a
keysc3 <colin.j.keys@gmail.com>
2023-07-23 20:14:33 -0600

    Bahri spell2 data.

---
+++ Assets/Scripts/ScriptableObjects/SpellData/Bahri/BahriSpell2Data.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BahriSpell2", menuName = "Spell/Bahri/Spell2")]
+public class BahriSpell2Data : SpellData
+{
+    [field: SerializeField] public GameObject missile { get; private set; }
+    [field: SerializeField] public ScriptableSpeedBonus spell_2_SpeedBonus { get; private set; }
+    [field: SerializeField] public float magnitude { get; private set; }
+    [field: SerializeField] public float rotationSpeed { get; private set; }
+    [field: SerializeField] public float heightOffset { get; private set; }
+    [field: SerializeField] public float duration { get; private set; }
+    [field: SerializeField] public float radius{ get; private set; }
+    [field: SerializeField] public float speed { get; private set; }
+    [field: SerializeField] public float multiplier { get; private set; }
+
+}
