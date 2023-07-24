0a34b03006e34bb9f9357b0af0c2225d4892f741
keysc3 <colin.j.keys@gmail.com>
2023-07-23 20:18:08 -0600

    Bahri spell3 data.

---
+++ Assets/Scripts/ScriptableObjects/SpellData/Bahri/BahriSpell3Data.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BahriSpell3", menuName = "Spell/Bahri/Spell3")]
+public class BahriSpell3Data : SpellData
+{
+    [field: SerializeField] public GameObject charmPrefab { get; private set; }
+    [field: SerializeField] public ScriptableCharm charmEffect { get; private set; }
+    [field: SerializeField] public float magnitude { get; private set; }
+    [field: SerializeField] public float speed { get; private set; }
+}
