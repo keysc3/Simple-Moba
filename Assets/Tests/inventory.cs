0485602d9da295870c12206a3133c003adad0a9e
keysc3 <colin.j.keys@gmail.com>
2023-08-02 15:00:07 -0600

    Error checking tests.

--- Assets/Tests/inventory.cs
+++ Assets/Tests/inventory.cs
@@ -28,12 +28,44 @@ public class inventory
         // Arrange
         Inventory inventory = new Inventory();
         Item item = ScriptableObject.CreateInstance<Item>();
+        int itemSlot = inventory.AddItem(item);
 
         // Act
-        int itemSlot = inventory.AddItem(item);
         Item removedItem = inventory.RemoveItem(itemSlot);
 
         // Assert
         Assert.AreEqual(item, removedItem);
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void add_more_items_than_max_inventory_size()
+    {
+        // Arrange
+        Inventory inventory = new Inventory();
+        for(int i = 0; i < inventory.space; i++){
+            inventory.AddItem(ScriptableObject.CreateInstance<Item>());
+        }
+
+        // Act
+        int itemSlot = inventory.AddItem(ScriptableObject.CreateInstance<Item>());
+
+        // Assert
+        Assert.AreEqual(-1, itemSlot);
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void remove_item_from_inventory_slot_without_item()
+    {
+        // Arrange
+        Inventory inventory = new Inventory();
+        inventory.AddItem(ScriptableObject.CreateInstance<Item>());
+
+        // Act
+        Item removedItem = inventory.RemoveItem(4);
+
+        // Assert
+        Assert.AreEqual(null, removedItem);
     }
 }
