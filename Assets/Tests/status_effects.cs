59b93fd346aa540992148f1c2e0284b86b1df05e
keysc3 <colin.j.keys@gmail.com>
2023-08-08 14:32:20 -0600

    Multiple effects with varying cc values test.

--- Assets/Tests/status_effects.cs
+++ Assets/Tests/status_effects.cs
@@ -6,8 +6,8 @@ using UnityEngine.TestTools;
 
 public class status_effects
 {
-    private List<float> slowValues { get;}= new List<float>(){0.1f, 0.15f, 0.2f, 0.25f, 0.3f};
-    private List<float> durationValues { get; }= new List<float>(){1f, 2f, 3f, 4f, 5f};
+    private List<float> slowValues { get; } = new List<float>(){0.1f, 0.15f, 0.2f, 0.25f, 0.3f};
+    private List<float> durationValues { get; } = new List<float>(){1f, 2f, 3f, 4f, 5f};
     /*public ScriptableSlow slow1;
     public ScriptableSlow slow2;*/
 
@@ -54,6 +54,63 @@ public class status_effects
 
         // Assert
         Assert.AreEqual(new List<bool>(){false, true}, isActivated);
+    }
+
+    [Test]
+    public void adds_range_of_cc_value_effects_and_sets_0_cc_values_active_and_strongest_nonzero_cc_value_active(){
+        // Arrange
+        GameObject g1 = new GameObject();
+        GameObject g2 = new GameObject();
+
+        ScriptableDrowsy drowsy = ScriptableDrowsy.CreateInstance<ScriptableDrowsy>();
+        drowsy.duration.AddRange(durationValues);
+        drowsy.slow = ScriptableObject.CreateInstance<ScriptableSlow>();
+        drowsy.slow.duration.AddRange(durationValues);
+        drowsy.slow.slowPercent.AddRange(slowValues);
+        drowsy.slow.name = "Drowsy Slow";
+        drowsy.sleep = ScriptableObject.CreateInstance<ScriptableSleep>();
+        drowsy.sleep.duration.AddRange(durationValues);
+        Drowsy drowsy1 = (Drowsy) drowsy.InitializeEffect(0, g1, g2);
+
+        ScriptableCharm charm = ScriptableCharm.CreateInstance<ScriptableCharm>();
+        charm.duration.AddRange(durationValues);
+        charm.slow = ScriptableObject.CreateInstance<ScriptableSlow>();
+        charm.slow.duration.AddRange(durationValues);
+        charm.slow.slowPercent.AddRange(slowValues);
+        Charm charm1 = (Charm) charm.InitializeEffect(4, g1, g2);
+
+        ScriptableSlow slow = ScriptableObject.CreateInstance<ScriptableSlow>();
+        slow.duration.AddRange(durationValues);
+        slow.slowPercent.AddRange(slowValues);
+        slow.name = "Slow";
+        Slow slow1 = (Slow) slow.InitializeEffect(2, g1, g2);
+
+        ScriptableSpeedBonus speedBonus = ScriptableObject.CreateInstance<ScriptableSpeedBonus>();
+        speedBonus.duration.AddRange(durationValues);
+        speedBonus.bonusPercent.AddRange(slowValues);
+        SpeedBonus speedBonus1 = (SpeedBonus) speedBonus.InitializeEffect(1, g1, g2);
+
+        ScriptableDot dot = ScriptableObject.CreateInstance<ScriptableDot>();
+        dot.duration.AddRange(durationValues);
+        Dot dot1 = (Dot) dot.InitializeEffect(10f, 1, g1, g2);
+
+        StatusEffects se = new StatusEffects();
+
+        // Act
+        se.AddEffect(drowsy1);
+        se.AddEffect(drowsy1.drowsySlow);
+        se.AddEffect(drowsy1.drowsySleep);
+        se.AddEffect(charm1);
+        se.AddEffect(charm1.charmSlow);
+        se.AddEffect(slow1);
+        se.AddEffect(speedBonus1);
+        se.AddEffect(dot1);
+        List<bool> isActivated = new List<bool>(){drowsy1.IsActivated, drowsy1.drowsySlow.IsActivated, drowsy1.drowsySleep.IsActivated, charm1.IsActivated, 
+        charm1.charmSlow.IsActivated, slow1.IsActivated, speedBonus1.IsActivated, dot1.IsActivated};
+
+        // Assert
+        Assert.AreEqual(new List<bool>(){true, false, true, false, true, false, true, true}, isActivated);
+
     }
 
     public Slow CreateSlowEffect(string slowName, int index){
