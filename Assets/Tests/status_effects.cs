01deb20bce4e8072998cc01d7a67c9781de3a830
keysc3 <colin.j.keys@gmail.com>
2023-08-08 15:00:19 -0600

    Multiple different 0 cc value effects activation test.

--- Assets/Tests/status_effects.cs
+++ Assets/Tests/status_effects.cs
@@ -32,7 +32,7 @@ public class status_effects
     }
 
     [Test]
-    public void sets_only_strongest_cc_effect_active(){
+    public void sets_only_strongest_cc_effect_active_from_nonzero_cc_effects(){
         // Arrange
         GameObject g1 = new GameObject();
         GameObject g2 = new GameObject();
@@ -79,11 +79,7 @@ public class status_effects
         charm.slow.slowPercent.AddRange(slowValues);
         Charm charm1 = (Charm) charm.InitializeEffect(4, g1, g2);
 
-        ScriptableSlow slow = ScriptableObject.CreateInstance<ScriptableSlow>();
-        slow.duration.AddRange(durationValues);
-        slow.slowPercent.AddRange(slowValues);
-        slow.name = "Slow";
-        Slow slow1 = (Slow) slow.InitializeEffect(2, g1, g2);
+        Slow slow1 = CreateSlowEffect("Slow1", 2);
 
         ScriptableSpeedBonus speedBonus = ScriptableObject.CreateInstance<ScriptableSpeedBonus>();
         speedBonus.duration.AddRange(durationValues);
@@ -111,6 +107,47 @@ public class status_effects
         // Assert
         Assert.AreEqual(new List<bool>(){true, false, true, false, true, false, true, true}, isActivated);
 
+    }
+
+    [Test]
+    public void adds_only_0_cc_values(){
+        // Arrange
+        GameObject g1 = new GameObject();
+        GameObject g2 = new GameObject();
+
+        Slow slow1 = CreateSlowEffect("Slow1", 1);
+
+        ScriptableSpeedBonus speedBonus = ScriptableObject.CreateInstance<ScriptableSpeedBonus>();
+        speedBonus.duration.AddRange(durationValues);
+        speedBonus.bonusPercent.AddRange(slowValues);
+        SpeedBonus speedBonus1 = (SpeedBonus) speedBonus.InitializeEffect(4, g1, g2);
+
+        ScriptableDot dot = ScriptableObject.CreateInstance<ScriptableDot>();
+        dot.duration.AddRange(durationValues);
+        Dot dot1 = (Dot) dot.InitializeEffect(20f, 3, g1, g2);
+
+        ScriptablePersonalSpell personalSpell1 = ScriptableObject.CreateInstance<ScriptablePersonalSpell>();
+        personalSpell1.duration.AddRange(durationValues);
+        PersonalSpell ps1 = (PersonalSpell) personalSpell1.InitializeEffect(2, g1, g2);
+
+    ScriptablePersonalSpell personalSpell2 = ScriptableObject.CreateInstance<ScriptablePersonalSpell>();
+        personalSpell2 = ScriptableObject.CreateInstance<ScriptablePersonalSpell>();
+        personalSpell2.duration.AddRange(new List<float>(){-1f});
+        PersonalSpell ps2 = (PersonalSpell) personalSpell2.InitializeEffect(0, g1, g2);
+
+        StatusEffects se = new StatusEffects();
+
+        //Act
+        se.AddEffect(slow1);
+        se.AddEffect(speedBonus1);
+        se.AddEffect(dot1);
+        se.AddEffect(ps1);
+        se.AddEffect(ps2);
+
+        List<bool> isActivated = new List<bool>(){slow1.IsActivated, speedBonus1.IsActivated, dot1.IsActivated, ps1.IsActivated, ps2.IsActivated};
+
+        // Assert
+        Assert.AreEqual(new List<bool>(){true, true, true, true, true}, isActivated);
     }
 
     public Slow CreateSlowEffect(string slowName, int index){
