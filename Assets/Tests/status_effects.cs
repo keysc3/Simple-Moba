a91365580b11d9ec5282398367aadf3e1677637a
keysc3 <colin.j.keys@gmail.com>
2023-08-08 13:02:46 -0600

    Strongest cc active only test.

--- Assets/Tests/status_effects.cs
+++ Assets/Tests/status_effects.cs
@@ -6,8 +6,8 @@ using UnityEngine.TestTools;
 
 public class status_effects
 {
-    private List<float> slowValues = new List<float>(){0.1f, 0.15f, 0.2f, 0.25f, 0.3f};
-    private List<float> durationValues = new List<float>(){1f, 2f, 3f, 4f, 5f};
+    private List<float> slowValues { get;}= new List<float>(){0.1f, 0.15f, 0.2f, 0.25f, 0.3f};
+    private List<float> durationValues { get; }= new List<float>(){1f, 2f, 3f, 4f, 5f};
     /*public ScriptableSlow slow1;
     public ScriptableSlow slow2;*/
 
@@ -29,6 +29,31 @@ public class status_effects
 
         // Assert
         Assert.AreEqual(new List<bool>(){false, true, false}, activeEffects);
+    }
+
+    [Test]
+    public void sets_only_strongest_cc_effect_active(){
+        // Arrange
+        GameObject g1 = new GameObject();
+        GameObject g2 = new GameObject();
+
+        ScriptableSleep sleep = ScriptableObject.CreateInstance<ScriptableSleep>();
+        sleep.duration.AddRange(durationValues);
+        Sleep sleep1 = (Sleep) sleep.InitializeEffect(3, g1, g2);
+
+        ScriptableCharm charm = ScriptableCharm.CreateInstance<ScriptableCharm>();
+        charm.duration.AddRange(durationValues);
+        Charm charm1 = (Charm) charm.InitializeEffect(4, g1, g2);
+
+        StatusEffects se = new StatusEffects();
+
+        // Act
+        se.AddEffect(charm1);
+        se.AddEffect(sleep1);
+        List<bool> isActivated = new List<bool>(){charm1.IsActivated, sleep1.IsActivated};
+
+        // Assert
+        Assert.AreEqual(new List<bool>(){false, true}, isActivated);
     }
 
     public Slow CreateSlowEffect(string slowName, int index){
