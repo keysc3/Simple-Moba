2048a70285467f3ce726b9506a2d118b8c3445ba
keysc3 <colin.j.keys@gmail.com>
2023-08-10 13:26:04 -0600

    Current health setter tests.

--- Assets/Tests/unit_stats.cs
+++ Assets/Tests/unit_stats.cs
@@ -6,16 +6,54 @@ using UnityEngine.TestTools;
 
 public class unit_stats
 {
+
     // A Test behaves as an ordinary method
     [Test]
-    public void unit_statsSimplePasses()
-    {
+    public void sets_current_health_to_62_with_100_max_health(){
         // Arrange
         ScriptableUnit sUnit = ScriptableObject.CreateInstance<ScriptableUnit>();
         UnitStats unitStats = new UnitStats(sUnit);
-
         unitStats.maxHealth.BaseValue = 100f;
 
+        // Act
+        unitStats.CurrentHealth = 62f;
+
+        // Assert 
+        Assert.AreEqual(62f, unitStats.CurrentHealth);
 
     }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void sets_current_health_to_max_health_given_value_greater_than_max_health(){
+        // Arrange
+        ScriptableUnit sUnit = ScriptableObject.CreateInstance<ScriptableUnit>();
+        UnitStats unitStats = new UnitStats(sUnit);
+        unitStats.maxHealth.BaseValue = 100f;
+        unitStats.CurrentHealth = 42f;
+
+        // Act
+        unitStats.CurrentHealth = 162f;
+
+        // Assert 
+        Assert.AreEqual(100f, unitStats.CurrentHealth);
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void sets_current_health_to_72_with_max_health_100_with_base_50_and_modifier_50(){
+        // Arrange
+        ScriptableUnit sUnit = ScriptableObject.CreateInstance<ScriptableUnit>();
+        UnitStats unitStats = new UnitStats(sUnit);
+        unitStats.maxHealth.BaseValue = 50f;
+        unitStats.maxHealth.AddModifier(50f);
+
+        // Act
+        unitStats.CurrentHealth = 72f;
+
+        // Assert 
+        Assert.AreEqual(72f, unitStats.CurrentHealth);
+    }
+
+
 }
