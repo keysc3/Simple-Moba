6a98bf13d55a658ec14c733905b0715a9c7094b8
keysc3 <colin.j.keys@gmail.com>
2023-08-25 15:11:28 -0600

    Documentation and cleanup.

--- Assets/Scripts/Unit/Damage/Damage.cs
+++ Assets/Scripts/Unit/Damage/Damage.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+* Purpose: Implements a damage class for creating objects to hold damage info.
+*
+* @author: Colin Keys
+*/
 public class Damage
 {
     public float amount { get; }
@@ -11,7 +16,7 @@ public class Damage
 
     /*
     *   Damage - Damage info object.
-    *   @param damageDealer - GameObject of the unit that dealt damage.
+    *   @param damageDealer - IUnit of the unit that dealt damage.
     *   @param damageAmount - float of the damage amount.
     *   @param damageType - string of the damage type.
     */
--- Assets/Scripts/Unit/Damage/DamageTracker.cs
+++ Assets/Scripts/Unit/Damage/DamageTracker.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+* Purpose: Implements damage tracking for assist granting and damage recap.
+*
+* @author: Colin Keys
+*/
 public class DamageTracker
 {
     public List<Damage> damageReceived { get; } = new List<Damage>();
+
     private float resetTime = 15f; 
     private float assistTime = 10.0f;
 
@@ -21,7 +27,7 @@ public class DamageTracker
 
     /*
     *   AddDamage - Adds a new Damage object to the damage tracker list.
-    *   @param damageDealer - GameObject of the damage source.
+    *   @param damageDealer - IUnit of the damage source.
     *   @param damageAmount - float of the amount of damage recieved.
     *   @param damageType - string of the type of damage.
     */
@@ -31,6 +37,8 @@ public class DamageTracker
 
     /*
     *   CheckForAssists - Checks if there are any eligible GameObjects to receive an assist.
+    *   @param killer - IPlayer of the killing unit.
+    *   @param currentTime - float of the current game time.
     *   @return List<GameObject> - List of GameObjects containing any damage dealers to this GameObject in the last 10s.
     */
     public List<IPlayer> CheckForAssists(IPlayer killer, float currentTime){
