4a4739a8694b9188f1ba4382cfe43b5243717eab
keysc3 <colin.j.keys@gmail.com>
2023-08-25 00:36:01 -0600

    Documentation and cleanup.

--- Assets/Scripts/Mocks/MockUnit.cs
+++ Assets/Scripts/Mocks/MockUnit.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+* Purpose: Implements an mock unit for testing.
+*
+* @author: Colin Keys
+*/
 public class MockUnit : IUnit
 {
     public ScriptableUnit SUnit { get; set; } = ScriptableObject.CreateInstance<ScriptableUnit>();
@@ -9,12 +14,7 @@ public class MockUnit : IUnit
     public StatusEffects statusEffects { get; set; }
     public DamageTracker damageTracker { get; set; }
     public Inventory inventory { get; set; }
-    //NewScore score { get; set; }
-    //LevelManager levelManager { get; set; }
-    //NavMeshAgent navMeshAgent { get; set; }
     public Collider myCollider { get; set; } = null;
-    //GameObject playerUI { get; set; }
-    //GameObject playerBar { get; set; }
     public bool IsDead { get; } = false;
     public BonusDamage bonusDamage { get; set; }
 }
