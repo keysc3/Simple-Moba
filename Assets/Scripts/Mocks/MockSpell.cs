6964dd441c18ed7d6a4fa39f1acd8a79b139fe96
keysc3 <colin.j.keys@gmail.com>
2023-08-29 12:40:39 -0600

    Mock spell.

---
+++ Assets/Scripts/Mocks/MockSpell.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MockSpell : ISpell
+{
+    public bool OnCd { get; set; }
+    public bool CanMove { get; set; }
+    public bool IsQuickCast { get; set; }
+    public bool IsDisplayed { get; set; }
+    public string SpellNum { get; set; }
+    public SpellData spellData { get; }
+    public Transform spellCDTransform { get; set; }
+    public TMP_Text spellCDText { get; set; }
+    public Image spellCDImage { get; set; }
+    public void DisplayCast(){}
+    public void HideCast(){}
+}
