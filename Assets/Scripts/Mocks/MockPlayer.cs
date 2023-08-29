c3bf5405b35a91f9c65c9ae37da1cbfd8097bf53
keysc3 <colin.j.keys@gmail.com>
2023-08-29 02:58:27 -0600

    Mock player IsDead not readonly.

--- Assets/Scripts/Mocks/MockPlayer.cs
+++ Assets/Scripts/Mocks/MockPlayer.cs
@@ -10,7 +10,7 @@ public class MockPlayer : IPlayer
     public DamageTracker damageTracker { get; set; }
     public Inventory inventory { get; set; }
     public Collider myCollider { get; set; }
-    public bool IsDead { get; }
+    public bool IsDead { get; set; }
     public BonusDamage bonusDamage { get; set; }
     public LevelManager levelManager { get; set; }
     public Score score { get; set; }
