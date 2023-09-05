9a4395b5dad798eb84ec2777f431d4dd7229bb32
keysc3 <colin.j.keys@gmail.com>
2023-09-05 15:06:30 -0600

    Left click tests.

--- Assets/Tests/spell_input_controller.cs
+++ Assets/Tests/spell_input_controller.cs
@@ -7,6 +7,9 @@ using System;
 
 public class spell_input_controller
 {
+
+    // Spell Button Press
+    
     // A Test behaves as an ordinary method
     [Test]
     public void does_not_cast_unlearned_spell()
@@ -170,5 +173,137 @@ public class spell_input_controller
         Tuple<ISpell, KeyCode> expected = new Tuple<ISpell, KeyCode>(null, KeyCode.None);
         Tuple<ISpell, KeyCode> actual = new Tuple<ISpell, KeyCode>(spellInput.LastSpellPressed, spellInput.LastButtonPressed);
         Assert.AreEqual(expected, actual);
+    }
+
+    // Left Click
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void does_not_unready_spell_from_no_last_spell_pressed()
+    {
+        // Arrange
+        MockSpellInput spellInput = new MockSpellInput();
+        SpellInputController controller = new SpellInputController(spellInput);
+        spellInput.LastButtonPressed = KeyCode.Q;
+        // Act
+        controller.LeftClick(new Ray(Vector3.zero, Vector3.right));
+
+        // Assert
+        Assert.AreEqual(KeyCode.Q, spellInput.LastButtonPressed);
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void does_not_unready_spell_from_button_click_left_click()
+    {
+        // Arrange
+        MockSpellInput spellInput = new MockSpellInput();
+        MockSpell spell = new MockSpell();
+        SpellInputController controller = new SpellInputController(spellInput);
+        spellInput.LastSpellPressed = spell;
+        spellInput.LastButtonPressed = KeyCode.Q;
+        spellInput.ButtonClick = true;
+
+        // Act
+        controller.LeftClick(new Ray(Vector3.zero, Vector3.right));
+
+        // Assert
+        Assert.AreEqual(KeyCode.Q, spellInput.LastButtonPressed);
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void hides_non_quick_cast_spells_cast_from_left_click()
+    {
+        // Arrange
+        MockSpellInput spellInput = new MockSpellInput();
+        MockSpell spell = new MockSpell();
+        SpellInputController controller = new SpellInputController(spellInput);
+        spell.IsDisplayed = true;
+        spellInput.LastSpellPressed = spell;
+        spellInput.LastButtonPressed = KeyCode.Q;
+
+        // Act
+        controller.LeftClick(new Ray(Vector3.zero, Vector3.right));
+
+        // Assert
+        Assert.AreEqual(false, spell.IsDisplayed);
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void unready_targeted_cast_spell_from_no_target_input()
+    {
+        // Arrange
+        MockSpellInput spellInput = new MockSpellInput();
+        MockTargetedCastSpell spell = new  MockTargetedCastSpell();
+        MockUnit unit = new MockUnit();
+        SpellInputController controller = new SpellInputController(spellInput);
+        unit.GameObject.AddComponent<BoxCollider>();
+        spell.SpellNum = "Not Casted";
+        spellInput.LastSpellPressed = spell;
+
+        // Act
+        controller.LeftClick(new Ray(Vector3.zero, Vector3.right));
+
+        // Assert
+        Assert.AreEqual(KeyCode.None, spellInput.LastButtonPressed);
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void casts_targeted_cast_spell()
+    {
+        // Arrange
+        MockSpellInput spellInput = new MockSpellInput();
+        MockTargetedCastSpell spell = new  MockTargetedCastSpell();
+        MockUnit unit = new MockUnit();
+        SpellInputController controller = new SpellInputController(spellInput);
+        unit.GameObject.AddComponent<BoxCollider>();
+        unit.GameObject.transform.position = Vector3.zero;
+        spell.SpellNum = "Not Casted";
+        spellInput.LastSpellPressed = spell;
+
+        // Act
+        controller.LeftClick(new Ray(Vector3.left, Vector3.right));
+
+        // Assert
+        Assert.AreEqual("Targeted spell casted", spell.SpellNum);
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void casts_non_quick_cast_spell()
+    {
+        // Arrange
+        MockSpellInput spellInput = new MockSpellInput();
+        MockCastableSpell spell = new  MockCastableSpell();
+        SpellInputController controller = new SpellInputController(spellInput);
+        spell.SpellNum = "Not Casted";
+        spellInput.LastSpellPressed = spell;
+
+        // Act
+        controller.LeftClick(new Ray(Vector3.zero, Vector3.right));
+
+        // Assert
+        Assert.AreEqual("Casted", spell.SpellNum);
+    }
+
+    // A Test behaves as an ordinary method
+    [Test]
+    public void unready_spell_without_cast()
+    {
+        // Arrange
+        MockSpellInput spellInput = new MockSpellInput();
+        MockSpell spell = new  MockSpell();
+        SpellInputController controller = new SpellInputController(spellInput);
+        spellInput.LastSpellPressed = spell;
+        spellInput.LastButtonPressed = KeyCode.Q;
+
+        // Act
+        controller.LeftClick(new Ray(Vector3.zero, Vector3.right));
+
+        // Assert
+        Assert.AreEqual(KeyCode.None, spellInput.LastButtonPressed);
     }
 }
