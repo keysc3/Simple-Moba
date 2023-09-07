90f61d4a90605b45b10465bc3c8b44f4bbfc3a5f
keysc3 <colin.j.keys@gmail.com>
2023-09-07 17:40:13 -0600

    Update tests to NSubstitute.

--- Assets/Tests/effect.cs
+++ Assets/Tests/effect.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using NSubstitute;
 
 public class effect
 {
@@ -10,12 +11,12 @@ public class effect
     [Test]
     public void does_not_set_effect_duration_to_given_invalid_negative_duration(){
         // Arrange
-        MockUnit m1 = new MockUnit();
-        MockUnit m2 = new MockUnit();
+        IUnit unit1 = Substitute.For<IUnit>();
+        IUnit unit2 = Substitute.For<IUnit>();
         ScriptableEffect sEffect = ScriptableObject.CreateInstance<ScriptableEffect>();
 
         // Act
-        Effect effect = new Effect(sEffect, -1.2f, m1, m2);
+        Effect effect = new Effect(sEffect, -1.2f, unit1, unit2);
 
         // Assert
         Assert.AreEqual(0f, effect.EffectDuration);
@@ -24,12 +25,12 @@ public class effect
     [Test]
     public void sets_effect_duration_to_5(){
         // Arrange
-        MockUnit m1 = new MockUnit();
-        MockUnit m2 = new MockUnit();
+        IUnit unit1 = Substitute.For<IUnit>();
+        IUnit unit2 = Substitute.For<IUnit>();
         ScriptableEffect sEffect = ScriptableObject.CreateInstance<ScriptableEffect>();
 
         // Act
-        Effect effect = new Effect(sEffect, 5f, m1, m2);
+        Effect effect = new Effect(sEffect, 5f, unit1, unit2);
 
         // Assert
         Assert.AreEqual(5f, effect.EffectDuration);
@@ -38,12 +39,12 @@ public class effect
     [Test]
     public void sets_effect_duration_to_negative_1(){
         // Arrange
-        MockUnit m1 = new MockUnit();
-        MockUnit m2 = new MockUnit();
+        IUnit unit1 = Substitute.For<IUnit>();
+        IUnit unit2 = Substitute.For<IUnit>();
         ScriptableEffect sEffect = ScriptableObject.CreateInstance<ScriptableEffect>();
 
         // Act
-        Effect effect = new Effect(sEffect, -1f, m1, m2);
+        Effect effect = new Effect(sEffect, -1f, unit1, unit2);
 
         // Assert
         Assert.AreEqual(-1f, effect.EffectDuration);
@@ -52,10 +53,10 @@ public class effect
     [Test]
     public void adds_2_seconds_to_effect_timer_with_duration_3(){
         // Arrange
-        MockUnit m1 = new MockUnit();
-        MockUnit m2 = new MockUnit();
+        IUnit unit1 = Substitute.For<IUnit>();
+        IUnit unit2 = Substitute.For<IUnit>();
         ScriptableEffect sEffect = ScriptableObject.CreateInstance<ScriptableEffect>();
-        Effect effect = new Effect(sEffect, 3f, m1, m2);
+        Effect effect = new Effect(sEffect, 3f, unit1, unit2);
 
         // Act
         effect.TimerTick(1.5f);
@@ -68,10 +69,10 @@ public class effect
     [Test]
     public void ends_effect_by_effect_timer_reaching_effect_duration(){
         // Arrange
-        MockUnit m1 = new MockUnit();
-        MockUnit m2 = new MockUnit();
+        IUnit unit1 = Substitute.For<IUnit>();
+        IUnit unit2 = Substitute.For<IUnit>();
         ScriptableEffect sEffect = ScriptableObject.CreateInstance<ScriptableEffect>();
-        Effect effect = new Effect(sEffect, 12.2f, m1, m2);
+        Effect effect = new Effect(sEffect, 12.2f, unit1, unit2);
 
         // Act
         effect.TimerTick(8.6f);
@@ -84,10 +85,10 @@ public class effect
     [Test]
     public void resets_effect_timer_from_11_to_0(){
         // Arrange
-        MockUnit m1 = new MockUnit();
-        MockUnit m2 = new MockUnit();
+        IUnit unit1 = Substitute.For<IUnit>();
+        IUnit unit2 = Substitute.For<IUnit>();
         ScriptableEffect sEffect = ScriptableObject.CreateInstance<ScriptableEffect>();
-        Effect effect = new Effect(sEffect, 11f, m1, m2);
+        Effect effect = new Effect(sEffect, 11f, unit1, unit2);
 
         // Act
         effect.ResetTimer();
@@ -99,16 +100,16 @@ public class effect
     [Test]
     public void overrides_effect_to_base_values_with_new_source(){
         // Arrange
-        MockUnit m1 = new MockUnit();
-        MockUnit m2 = new MockUnit();
-        MockUnit m3 = new MockUnit();
+        IUnit unit1 = Substitute.For<IUnit>();
+        IUnit unit2 = Substitute.For<IUnit>();
+        IUnit unit3 = Substitute.For<IUnit>();
         ScriptableEffect sEffect = ScriptableObject.CreateInstance<ScriptableEffect>();
-        Effect effect = new Effect(sEffect, 4f, m1, m2);
+        Effect effect = new Effect(sEffect, 4f, unit1, unit2);
 
         // Act
-        effect.OverrideEffect(m3);
+        effect.OverrideEffect(unit3);
 
         // Assert
-        Assert.AreEqual((0f, false, (IUnit) m3), (effect.effectTimer, effect.isFinished, effect.casted));
+        Assert.AreEqual((0f, false, (IUnit) unit3), (effect.effectTimer, effect.isFinished, effect.casted));
     }
 }
