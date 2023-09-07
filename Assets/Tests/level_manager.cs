277968dd3ea6677038d739756febb32ea9a8b5e5
keysc3 <colin.j.keys@gmail.com>
2023-09-07 17:36:55 -0600

    Update tests to NSubstitute.

--- Assets/Tests/level_manager.cs
+++ Assets/Tests/level_manager.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using System.Linq;
+using NSubstitute;
 
 public class level_manager
 {
@@ -14,10 +15,8 @@ public class level_manager
     public void gain_xp_from_player_kill()
     {
         // Arrange
-        MockPlayer player = new MockPlayer();
-        MockPlayer enemy = new MockPlayer();
-        player.levelManager = new LevelManager(player);
-        enemy.levelManager = new LevelManager(enemy);
+        IPlayer player = CreateMockPlayerWithLevelManager(1);
+        IPlayer enemy = CreateMockPlayerWithLevelManager(1);
 
         // Act
         player.levelManager.GainXP(enemy);
@@ -30,8 +29,8 @@ public class level_manager
     public void gain_xp_from_non_player_unit_kill()
     {
         // Arrange
-        MockPlayer player = new MockPlayer();
-        MockUnit unit = new MockUnit();
+        IPlayer player = CreateMockPlayerWithLevelManager(1);
+        IUnit unit = Substitute.For<IUnit>();
         player.levelManager = new LevelManager(player);
 
         // Act
@@ -45,8 +44,8 @@ public class level_manager
     public void validates_overflow_xp_from_level_up()
     {
         // Arrange
-        MockPlayer player = new MockPlayer();
-        MockPlayer enemy = new MockPlayer();
+        IPlayer player = CreateMockPlayerWithLevelManager(1);
+        IPlayer enemy = CreateMockPlayerWithLevelManager(1);
         player.levelManager = new LevelManager(player);
         enemy.levelManager = new LevelManager(enemy);
         float totalXP = 0f;
@@ -69,8 +68,8 @@ public class level_manager
     public void level_up_from_level_one_to_three_without_unit_stats()
     {
         // Arrange
-        MockPlayer player = new MockPlayer();
-        MockPlayer enemy = new MockPlayer();
+        IPlayer player = CreateMockPlayerWithLevelManager(1);
+        IPlayer enemy = CreateMockPlayerWithLevelManager(1);
         player.levelManager = new LevelManager(player);
         enemy.levelManager = new LevelManager(enemy);
 
@@ -87,21 +86,20 @@ public class level_manager
     public void update_unit_stats_from_leveling_from_one_to_four()
     {
         // Arrange
-        MockPlayer player = new MockPlayer();
-        ((ScriptableChampion) player.SUnit).healthGrowth = 63f;
-        ((ScriptableChampion) player.SUnit).manaGrowth = 21f;
-        ((ScriptableChampion) player.SUnit).physicalDamageGrowth = 5.2f;
-        ((ScriptableChampion) player.SUnit).attackSpeedGrowth = 1.8f;
-        player.levelManager = new LevelManager(player);
-        player.unitStats = new ChampionStats((ScriptableChampion) player.SUnit);
-        player.unitStats.maxHealth.BaseValue = 100f;
-        player.unitStats.CurrentHealth = 60f;
-        ((ChampionStats) player.unitStats).maxMana.BaseValue = 50f;
-        ((ChampionStats) player.unitStats).CurrentMana = 26f;
-        player.unitStats.physicalDamage.BaseValue = 39f;
-
-        MockPlayer enemy = new MockPlayer();
-        enemy.levelManager = new LevelManager(enemy);
+        IPlayer player = CreateMockPlayerWithLevelManager(1);
+        ScriptableChampion champ = ScriptableObject.CreateInstance<ScriptableChampion>();
+        champ.healthGrowth = 63f;
+        champ.manaGrowth = 21f;
+        champ.physicalDamageGrowth = 5.2f;
+        champ.attackSpeedGrowth = 1.8f;
+        player.SUnit.Returns(champ);
+        ChampionStats unitStats = new ChampionStats(champ);
+        unitStats.maxHealth.BaseValue = 100f;
+        unitStats.CurrentHealth = 60f;
+        unitStats.maxMana.BaseValue = 50f;
+        unitStats.CurrentMana = 26f;
+        unitStats.physicalDamage.BaseValue = 39f;
+        player.unitStats.Returns(unitStats);
         
         // Act
         while(player.levelManager.Level < 4){
@@ -110,17 +108,16 @@ public class level_manager
         
         // Assert
         List<float> expected = new List<float>(){242.695f, 202.695f, 97.565f, 73.565f, 50.778f, 4.077f};
-        List<float> actual = new List<float>(){player.unitStats.maxHealth.BaseValue, player.unitStats.CurrentHealth, 
-        ((ChampionStats) player.unitStats).maxMana.BaseValue, ((ChampionStats) player.unitStats).CurrentMana, player.unitStats.physicalDamage.BaseValue, 
-        player.unitStats.bonusAttackSpeed.BaseValue};
+        List<float> actual = new List<float>(){unitStats.maxHealth.BaseValue, unitStats.CurrentHealth, 
+        unitStats.maxMana.BaseValue, unitStats.CurrentMana, unitStats.physicalDamage.BaseValue, 
+        unitStats.bonusAttackSpeed.BaseValue};
         Assert.IsTrue(expected.SequenceEqual(actual, new FloatComparer()));
     }
 
     [Test]
     public void levels_up_spell_3_to_level_1(){
         // Arrange
-        MockPlayer player = new MockPlayer();
-        player.levelManager = new LevelManager(player);
+        IPlayer player = CreateMockPlayerWithLevelManager(1);
         
         // Act
         player.levelManager.SpellLevelUpRequest("Spell_3");
@@ -132,9 +129,8 @@ public class level_manager
     [Test]
     public void does_not_level_up_spell_1_past_max_level(){
         // Arrange
-        MockPlayer player = new MockPlayer();
         int maxSpellLevel = levelInfo.maxSpellLevel;
-        player.levelManager = new LevelManager(player, maxSpellLevel);
+        IPlayer player = CreateMockPlayerWithLevelManager(maxSpellLevel);
         
         // Act
         for(int i = 0; i < maxSpellLevel+1; i++){
@@ -148,8 +144,7 @@ public class level_manager
     [Test]
     public void does_not_level_up_spell_2_to_level_1_from_insufficient_skill_points(){
         // Arrange
-        MockPlayer player = new MockPlayer();
-        player.levelManager = new LevelManager(player);
+        IPlayer player = CreateMockPlayerWithLevelManager(1);
         player.levelManager.SpellLevelUpRequest("Spell_3");
 
         // Act
@@ -162,8 +157,7 @@ public class level_manager
     [Test]
     public void does_not_level_spell_4_to_level_1_from_invalid_level_requirements(){
         // Arrange
-        MockPlayer player = new MockPlayer();
-        player.levelManager = new LevelManager(player, 4);
+        IPlayer player = CreateMockPlayerWithLevelManager(4);
 
         // Act
         player.levelManager.SpellLevelUpRequest("Spell_4");
@@ -175,8 +169,7 @@ public class level_manager
     [Test]
     public void levels_spell_4_to_level_1(){
         // Arrange
-        MockPlayer player = new MockPlayer();
-        player.levelManager = new LevelManager(player, 6);
+        IPlayer player = CreateMockPlayerWithLevelManager(6);
 
         // Act
         player.levelManager.SpellLevelUpRequest("Spell_4");
@@ -188,8 +181,7 @@ public class level_manager
     [Test]
     public void does_not_level_spell_4_to_level_2_from_invalid_level_requirements(){
         // Arrange
-        MockPlayer player = new MockPlayer();
-        player.levelManager = new LevelManager(player, 10);
+        IPlayer player = CreateMockPlayerWithLevelManager(10);
 
         // Act
         player.levelManager.SpellLevelUpRequest("Spell_4");
@@ -202,8 +194,7 @@ public class level_manager
     [Test]
     public void levels_spell_4_to_level_2(){
         // Arrange
-        MockPlayer player = new MockPlayer();
-        player.levelManager = new LevelManager(player, 11);
+        IPlayer player = CreateMockPlayerWithLevelManager(11);
 
         // Act
         player.levelManager.SpellLevelUpRequest("Spell_4");
@@ -216,8 +207,7 @@ public class level_manager
     [Test]
     public void does_not_level_spell_4_to_level_3_from_invalid_level_requirements(){
         // Arrange
-        MockPlayer player = new MockPlayer();
-        player.levelManager = new LevelManager(player, 15);
+        IPlayer player = CreateMockPlayerWithLevelManager(15);
 
         // Act
         for(int i = 0; i < 3; i++){
@@ -231,8 +221,7 @@ public class level_manager
     [Test]
     public void levels_spell_4_to_level_3(){
         // Arrange
-        MockPlayer player = new MockPlayer();
-        player.levelManager = new LevelManager(player, 16);
+        IPlayer player = CreateMockPlayerWithLevelManager(16);
 
         // Act
         for(int i = 0; i < 3; i++){
@@ -246,9 +235,8 @@ public class level_manager
     [Test]
     public void does_not_level_up_spell_4_past_max_level(){
         // Arrange
-        MockPlayer player = new MockPlayer();
         int maxUltLevel = levelInfo.maxUltLevel;
-        player.levelManager = new LevelManager(player, 17);
+        IPlayer player = CreateMockPlayerWithLevelManager(17);
         
         // Act
         for(int i = 0; i < maxUltLevel+1; i++){
@@ -262,8 +250,7 @@ public class level_manager
     [Test]
     public void gets_respawn_time_from_player_under_level_7(){
         // Arrange
-        MockPlayer player = new MockPlayer();
-        player.levelManager = new LevelManager(player, 4);
+        IPlayer player = CreateMockPlayerWithLevelManager(4);
 
         // Act
         float respawnTime = player.levelManager.RespawnTime();
@@ -275,8 +262,7 @@ public class level_manager
     [Test]
     public void gets_respawn_time_from_player_at_level_7(){
         // Arrange
-        MockPlayer player = new MockPlayer();
-        player.levelManager = new LevelManager(player, 7);
+        IPlayer player = CreateMockPlayerWithLevelManager(7);
 
         // Act
         float respawnTime = player.levelManager.RespawnTime();
@@ -288,14 +274,22 @@ public class level_manager
     [Test]
     public void gets_respawn_time_from_player_above_level_7(){
         // Arrange
-        MockPlayer player = new MockPlayer();
-        player.levelManager = new LevelManager(player, 15);
+        IPlayer player = CreateMockPlayerWithLevelManager(15);
 
         // Act
         float respawnTime = player.levelManager.RespawnTime();
 
         // Assert
         Assert.AreEqual(45f, respawnTime);
+    }
+
+    private IPlayer CreateMockPlayerWithLevelManager(int level){
+        IPlayer player = Substitute.For<IPlayer>();
+        if(level > 1)
+            player.levelManager = new LevelManager(player, level);
+        else
+            player.levelManager = new LevelManager(player);
+        return player;
     }
 
 }
