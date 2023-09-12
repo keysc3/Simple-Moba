908e64a69d752c2eca74263a84013760e0edeaf7
keysc3 <colin.j.keys@gmail.com>
2023-09-11 18:24:20 -0600

    Health UI tests.

--- Assets/PlayTests/user_interface.cs
+++ Assets/PlayTests/user_interface.cs
@@ -17,8 +17,6 @@ public class user_interface
         GameObject parent = CreateParentChild();
         parent.transform.GetChild(0).gameObject.AddComponent<TextMeshProUGUI>();
         Slider slider = CreateSlider(80f, parent);
-        MockPlayer player = new MockPlayer();
-        player.unitStats = new ChampionStats(ScriptableObject.CreateInstance<ScriptableChampion>());
         UpdateManaUI manaUI = parent.AddComponent<UpdateManaUI>();
         // Use yield to skip a frame.
         yield return null;
@@ -51,7 +49,7 @@ public class user_interface
     {
         // Use the Assert class to test conditions.
         GameObject parent = CreateParentChild();
-        parent.transform.GetChild(0).gameObject.AddComponent<TextMeshProUGUI>();
+        TMP_Text text = parent.transform.GetChild(0).gameObject.AddComponent<TextMeshProUGUI>();
         Slider slider = CreateSlider(0.8f, parent);
         MockPlayer player = new MockPlayer();
         ChampionStats stats = new ChampionStats(ScriptableObject.CreateInstance<ScriptableChampion>());
@@ -62,7 +60,7 @@ public class user_interface
         manaUI.player = player;
         // Use yield to skip a frame.
         yield return null;
-        Assert.AreEqual(40f, slider.value);
+        Assert.AreEqual((40f, "40/100"), (slider.value, text.text));
     }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
@@ -112,6 +110,61 @@ public class user_interface
         // Use yield to skip a frame.
         yield return null;
         Assert.AreEqual(15f, slider.value);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator does_not_update_health_ui_with_null_player()
+    {
+        // Use the Assert class to test conditions.
+        GameObject parent = CreateParentChild();
+        parent.transform.GetChild(0).gameObject.AddComponent<TextMeshProUGUI>();
+        Slider slider = CreateSlider(80f, parent);
+        parent.AddComponent<UpdateHealthUI>();
+        // Use yield to skip a frame.
+        yield return null;
+        Assert.AreEqual(80f, slider.value);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator updates_health_ui_to_0_current_health_from_dead_player()
+    {
+        // Use the Assert class to test conditions.
+        GameObject parent = CreateParentChild();
+        parent.transform.GetChild(0).gameObject.AddComponent<TextMeshProUGUI>();
+        Slider slider = CreateSlider(50f, parent);
+        MockPlayer player = new MockPlayer();
+        player.IsDead = true;
+        player.unitStats = new ChampionStats(ScriptableObject.CreateInstance<ScriptableChampion>());
+        UpdateHealthUI healthUI = parent.AddComponent<UpdateHealthUI>();
+        healthUI.player = player;
+        // Use yield to skip a frame.
+        yield return null;
+        Assert.AreEqual(0f, slider.value);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator updates_health_ui_to_current_health_percent()
+    {
+        // Use the Assert class to test conditions.
+        GameObject parent = CreateParentChild();
+        TMP_Text text = parent.transform.GetChild(0).gameObject.AddComponent<TextMeshProUGUI>();
+        Slider slider = CreateSlider(64f, parent);
+        MockPlayer player = new MockPlayer();
+        ChampionStats stats = new ChampionStats(ScriptableObject.CreateInstance<ScriptableChampion>());
+        stats.maxHealth.BaseValue = 100f;
+        stats.CurrentHealth = 98f;
+        player.unitStats = stats;
+        UpdateHealthUI healthUI = parent.AddComponent<UpdateHealthUI>();
+        healthUI.player = player;
+        // Use yield to skip a frame.
+        yield return null;
+        Assert.AreEqual((98f, "98/100"), (slider.value, text.text));
     }
 
     private Slider CreateSlider(float startingValue, GameObject g1){
