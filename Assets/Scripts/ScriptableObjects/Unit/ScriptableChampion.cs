694a8a8a713af685a845c24c92cf6acc1b164812
keysc3 <colin.j.keys@gmail.com>
2023-09-20 15:35:24 -0600

    Remove redundant values in champion scriptable object.

--- Assets/Scripts/ScriptableObjects/Unit/ScriptableChampion.cs
+++ Assets/Scripts/ScriptableObjects/Unit/ScriptableChampion.cs
@@ -11,14 +11,6 @@ using UnityEngine;
 
 public class ScriptableChampion : ScriptableUnit
 {
-    #region "Sprites"
-    [field: SerializeField] public Sprite passive_sprite { get; private set; }
-    [field: SerializeField] public Sprite spell_1_sprite { get; private set; }
-    [field: SerializeField] public Sprite spell_2_sprite { get; private set; }
-    [field: SerializeField] public Sprite spell_3_sprite { get; private set; }
-    [field: SerializeField] public Sprite spell_4_sprite { get; private set; }
-    #endregion
-
     #region "Base Stats"
     [field: SerializeField] public float baseMana { get; private set; }
     [field: SerializeField] public float MP5 { get; private set; }
@@ -33,31 +25,5 @@ public class ScriptableChampion : ScriptableUnit
     [field: SerializeField] public float armorGrowth { get; set; }
     [field: SerializeField] public float magicResistGrowth { get; set; }
     [field: SerializeField] public float attackSpeedGrowth { get; set; }
-    #endregion
-
-    #region "Spell Values Per Level"
-    //dmg, cd, cost
-    [field: SerializeField] public List<float> spell1BaseDamage { get; private set; }
-    [field: SerializeField] public List<float> spell1BaseCd { get; private set; }
-    [field: SerializeField] public List<float> spell1BaseMana { get; private set; }
-
-    [field: SerializeField] public List<float> spell2BaseDamage { get; private set; }
-    [field: SerializeField] public List<float> spell2BaseCd { get; private set; }
-    [field: SerializeField] public List<float> spell2BaseMana { get; private set; }
-
-    [field: SerializeField] public List<float> spell3BaseDamage { get; private set; }
-    [field: SerializeField] public List<float> spell3BaseCd { get; private set; }
-    [field: SerializeField] public List<float> spell3BaseMana { get; private set; }
-
-    [field: SerializeField] public List<float> spell4BaseDamage { get; private set; }
-    [field: SerializeField] public List<float> spell4BaseCd { get; private set; }
-    [field: SerializeField] public List<float> spell4BaseMana { get; private set; }
-    #endregion
-
-    #region "Spell Attributes"
-    [field: SerializeField] public float spell_1_castTime { get; private set; }
-    [field: SerializeField] public float spell_2_castTime { get; private set; }
-    [field: SerializeField] public float spell_3_castTime { get; private set; }
-    [field: SerializeField] public float spell_4_castTime { get; private set; }
     #endregion
 }
