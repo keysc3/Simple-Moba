9d742a01389690618f288d4287a8da92ef69086c
keysc3 <colin.j.keys@gmail.com>
2023-09-20 17:17:53 -0600

    Update ghost speed bonus usage.

--- Assets/Scripts/Spell/SummonerSpells/Ghost.cs
+++ Assets/Scripts/Spell/SummonerSpells/Ghost.cs
@@ -33,8 +33,7 @@ public class Ghost : Spell, IHasCast
     *   Cast - Casts the spell.
     */
     public void Cast(){
-        spellData.speedBonus.bonusPercent[0] = CalculateSpeedBonus();
-        player.statusEffects.AddEffect(spellData.speedBonus.InitializeEffect(0, player, player));
+        player.statusEffects.AddEffect(spellData.speedBonus.InitializeEffect(0, CalculateSpeedBonus(), player, player));
         OnCd = true;
         StartCoroutine(spellController.Spell_Cd_Timer(spellData.baseCd[0]));
     }
