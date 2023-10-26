fe8c34f4f7922b344f3430fdb74f9cce0c3a23f3
keysc3 <colin.j.keys@gmail.com>
2023-10-26 02:04:24 -0600

    Spell type enum to replace strings.

---
+++ Assets/Scripts/Enums/SpellType.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpellType
+{
+    None,
+    Passive,
+    Spell1,
+    Spell2,
+    Spell3,
+    Spell4,
+    SummonerSpell1,
+    SummonerSpell2
+}
