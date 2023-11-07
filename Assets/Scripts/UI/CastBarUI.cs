7c08a8d2095ec9d8d66e1242da128f2e0fa33a56
keysc3 <colin.j.keys@gmail.com>
2023-11-06 20:07:44 -0700

    File rename and object it is attached to changed.

--- Assets/Scripts/UI/CastBarUI.cs
+++ Assets/Scripts/UI/CastBarUI.cs
@@ -4,28 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class SpellUI : MonoBehaviour
+public class CastBarUI : MonoBehaviour
 {
-    private GameObject castBar;
     private Slider castBarSlider;
     private TMP_Text castBarText;
 
     // Start is called before the first frame update
     void Start()
     {
-        castBar = transform.Find("CastbarContainer").gameObject;
-        castBarSlider = castBar.transform.Find("Castbar").GetComponent<Slider>();
-        castBarText = castBar.transform.Find("Spell").GetComponent<TMP_Text>();
+        castBarSlider = transform.Find("Castbar").GetComponent<Slider>();
+        castBarText = transform.Find("Spell").GetComponent<TMP_Text>();
         Spell[] objSpells = GetComponentsInParent<Spell>();
         foreach(Spell spell in objSpells){
             spell.spellController.CastBarUpdateCallback += CastBarUpdate;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gameObject.SetActive(false);
     }
 
     /*
@@ -33,12 +26,11 @@ public class SpellUI : MonoBehaviour
     *   @param castTime - float for the duration to stop the champion for casting.
     */
     private void CastBarUpdate(float timer, ISpell spell){
-        Debug.Log(timer + " | " + spell.spellData.castTime);
         if(spell.spellData.castTime > 0 && timer >= spell.spellData.castTime)
-            castBar.SetActive(false);
+            gameObject.SetActive(false);
         else
-            if(!castBar.activeSelf)
-                castBar.SetActive(true);
+            if(!gameObject.activeSelf)
+                gameObject.SetActive(true);
             if(castBarText.text != spell.spellData.name)
                 castBarText.text = spell.spellData.name;
             castBarSlider.value = Mathf.Clamp(timer/spell.spellData.castTime, 0f, 1f);
